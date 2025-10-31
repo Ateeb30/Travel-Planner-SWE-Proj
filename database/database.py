dbb8f7d8f8cb171b6ebb61cc085418574b34acde
Your Name <you@example.com>
2025-10-31 13:32:43 +0500

    Added db tables: users, trips, and admins

--- database/database.py
+++ database/database.py
@@ -1,16 +1,20 @@
 from peewee import (
-    Model, CharField, AutoField
+    Model, CharField, AutoField, IntegerField, ForeignKeyField, DateField
 )
 from playhouse.pool import PooledPostgresqlDatabase
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 try:
     db = PooledPostgresqlDatabase(
-        'travel_planner_swe',   
-        user='postgres',      
-        password='Minato12!',        
-        host='localhost',
-        port=5432
-    )
+        os.getenv('DB_NAME', 'travel_planner_swe'),
+        user=os.getenv('DB_USER', 'postgres'),
+        password=os.getenv('DB_PASSWORD'),
+        host=os.getenv('DB_HOST', 'localhost'),
+        port=int(os.getenv('DB_PORT', 5432))
+)
     db.connect()
     print("Connected to PostgreSQL successfully!")
 
@@ -36,7 +40,28 @@ class User(BaseModel):
         table_name = 'users'
 
 
+class Trip(BaseModel):
+    trip_id = AutoField(primary_key=True)
+    maxBudget = IntegerField()
+    destination = CharField(max_length=100)
+    startDate = DateField()
+    endDate = DateField()
+    user = ForeignKeyField(User, backref='trips', on_delete='CASCADE')
+
+    class Meta:
+        table_name = 'trips'
+
+class Admin(BaseModel):
+    admin_id = AutoField(primary_key=True)
+    user_name = CharField(max_length=20)
+    password = CharField(max_length=100, null=True)
+    access_level = IntegerField()
+    user = ForeignKeyField(User, backref='trips', on_delete='CASCADE')
+
+    class Meta:
+        table_name = 'admin'
+
 if __name__ == "__main__":
-    db.create_tables([User])
-    print("✅ Table 'users' created successfully!")
+    db.create_tables([User, Trip])
+    print("Tables created successfully!")
     db.close()
